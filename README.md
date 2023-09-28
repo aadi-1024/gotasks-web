4ff60e89c61f2ac8e989b62258f58dd2e1dadb5c
Aaditya Thakur <aaditya@pop-os.localdomain>
2023-09-28 17:56:10 +0530

    basic html templating

---
+++ README.md
@@ -0,0 +1,3 @@
+### Gotasks-web
+
+Same ol' todo app but on the web with everything from routing to HTML templating written using Go's stdlib without any third party modules.
--- api/api.go
+++ api/api.go
@@ -1,16 +1,34 @@
 package api
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 	"os"
 )
 
-var htmlTemplates map[string][]byte
+type Entry struct {
+	Id   int
+	Name string
+	Time string
+}
+
+type PageData struct {
+	PageTitle string
+	Entries   []Entry
+}
+
+var tmpl *template.Template
 
 func loadTemplates() error {
-	htmlTemplates = make(map[string][]byte)
+
+	tmpl = template.New("index")
+
 	content, err := os.ReadFile("templates/index.html")
-	htmlTemplates["index"] = content
+
+	tmpl = template.Must(tmpl.Parse(string(content)))
+	//test data
+
 	return err
 
 }
@@ -22,12 +40,42 @@ func AddRoutes(handler *http.ServeMux) error {
 		return err
 	}
 
+	pageData := PageData{
+		PageTitle: "Ayo this works",
+		Entries: []Entry{
+			Entry{
+				Id:   0,
+				Name: "Do this",
+				Time: "12:00",
+			},
+			Entry{
+				Id:   1,
+				Name: "Do that",
+				Time: "1:00",
+			},
+		},
+	}
+
 	//index
 	handler.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Add("content-type", "text/html")
 		writer.Header().Add("connection", "close")
 		writer.WriteHeader(200)
-		_, _ = writer.Write(htmlTemplates["index"])
+	})
+
+	handler.HandleFunc("/task", func(writer http.ResponseWriter, request *http.Request) {
+
+		switch request.Method {
+		case "GET":
+			//writer.Header().Add("content-type", "text/html")
+			//writer.Header().Add("connection", "close")
+			//writer.WriteHeader(200)
+			//_, _ = writer.Write([]byte(fmt.Sprintf("<h1>%v</h1>", request.URL.Query()["id"])))
+			err = tmpl.Execute(writer, pageData)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	})
 
 	return nil
